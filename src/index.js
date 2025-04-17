cf24d6fdba4ebbfa9107681019f041d9b989f5f5
MinsandiDeSilva <minsandidesilva21@gmail.com>
2025-04-17 13:49:11 +0530

    week 10

--- src/index.js
+++ src/index.js
@@ -4,16 +4,13 @@ import jobApplicationsRouter from "./api/jobApplications.js";
 import "dotenv/config";
 import { connectDB } from "./persistance/db.js";
 import globalErrorHandlingMiddleware from "./api/middleware/global-error-handling-middleware.js";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
+app.use(cors({ origin: "http://localhost:5173" }));
 
 connectDB();
-
-// app.use((req, res, next) => {
-//   console.log("HI from the top level middleware");
-//   next();
-// });
 
 app.use("/api/jobs", jobsRouter);
 app.use("/api/jobapplications", jobApplicationsRouter);
