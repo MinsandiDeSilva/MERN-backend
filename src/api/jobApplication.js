7b4d0d16bc56f643a031119f70143421a97ebc9c
MinsandiDeSilva <minsandidesilva21@gmail.com>
2025-04-17 17:57:52 +0530

    Fixed Errors

--- src/api/jobApplication.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import {createJobApplication,getAllJobApplications} from "./../application/jobApplications.js"
-
-const jobApplicationsRouter = express.Router();
-
-jobApplicationsRouter.route('/').post(createJobApplication).get(getAllJobApplications)
-
-export default jobApplicationsRouter;
---
+++ src/api/jobApplications.js
@@ -0,0 +1,20 @@
+import express from "express";
+import {
+  createJobApplication,
+  getAllJobApplications,
+} from "../application/jobApplications.js";
+import { ClerkExpressRequireAuth } from "@clerk/clerk-sdk-node";
+import AuthorizationMiddleware from "./middleware/authorization-middleware.js";
+
+const jobApplicationsRouter = express.Router();
+
+jobApplicationsRouter
+  .route("/")
+  .post(ClerkExpressRequireAuth({}), createJobApplication)
+  .get(
+    ClerkExpressRequireAuth({}),
+    AuthorizationMiddleware,
+    getAllJobApplications
+  );
+
+export default jobApplicationsRouter;
