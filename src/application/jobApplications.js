c76da96ce88d57a902bbb6762855b673aa5f1261
MinsandiDeSilva <minsandidesilva21@gmail.com>
2025-04-18 23:40:42 +0530

    Fixed errors

--- src/application/jobApplications.js
+++ src/application/jobApplications.js
@@ -1,10 +1,18 @@
 import ValidationError from "../domain/errors/validation-error.js";
 import JobApplication from "../persistance/entities/jobApplications.js";
 import { JobApplicationDTO } from "./dto/jobApplications.js";
-import { clerkClient } from "@clerk/clerk-sdk-node";
 
 export const getAllJobApplications = async (req, res, next) => {
   try {
+    //?jobId=${jobId}
+    const { jobId } = req.query;
+    if (jobId) {
+      const jobApplications = await JobApplication.find({ job: jobId })
+        .populate("job", ["title", "description"])
+        .exec();
+      return res.status(200).json(jobApplications);
+    }
+
     const jobApplications = await JobApplication.find()
       .populate("job", ["title", "description"])
       .exec();
@@ -28,8 +36,30 @@ export const createJobApplication = async (req, res, next) => {
       throw new ValidationError(jobApplication.error);
     }
 
-    await JobApplication.create({ ...jobApplication.data, userId: userId });
+    const createdJobApplication = await JobApplication.create({ ...jobApplication.data, userId: userId });
+    //to generate rating
+    //const rating = generateRating(createdJobApplication)
+
+    
+    
+
     return res.status(201).send();
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getJobApplicationById = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const jobApplication = await JobApplication.findById(id).populate("job", [
+      "title",
+      "description",
+    ]);
+    if (jobApplication === null) {
+      throw new NotFoundError("Job Application not found");
+    }
+    return res.status(200).json(jobApplication);
   } catch (error) {
     next(error);
   }
